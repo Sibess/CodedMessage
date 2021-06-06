1747e08c2267eb8fbd3a736b6904ff1a2426c6a7
Siarhei <taler34@gmail.com>
2021-06-07 00:15:01 +0300

    CodedMessage

---
+++ CodedMessage/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CodedMessage
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HackerSpeak("javascript is cool"));
+        }
+
+        public static string HackerSpeak(string str)
+        {
+           string replacedMessage= str.Replace("a", "4").Replace("e", "3").Replace("i", "1").Replace("o", "0").Replace("s", "5");
+           return replacedMessage;
+        }
+    }
+}
